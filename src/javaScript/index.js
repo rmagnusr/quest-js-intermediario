abcc80542834318eda8e9653e1d48f6867579f0d
rmagnusr <rmagnusdarosa@gmail.com>
2023-02-23 13:06:52 -0300

    realizada a responsividade, além de ajustes na lógica do formulário

--- src/javaScript/index.js
+++ src/javaScript/index.js
@@ -1,6 +1,42 @@
 //coletando os elementos
 
 const form = document.getElementById('formulario')
+const campos = document.querySelectorAll('.campo')
+const alertas = document.querySelectorAll('.alerta')
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault()
+  campos.forEach((campos, index) => {
+
+  if (campos.value == ""){
+    alertas[index].classList.add('visivel')
+  campos.classList.add('invalid')
+}else {
+  campos.classList.remove('invalid')
+    campos.classList.add('valid')
+    alertas[index].classList.remove('visivel')
+   
+}
+  })
+})
+
+  campos.forEach((input, index) => {
+input.addEventListener('input', function (event){
+  if (input.value == ""){
+    
+    input.classList.add("invalid")
+    input.classList.remove("valid")
+    alertas[index].classList.add('visivel')
+  } else {
+    input.classList.remove("invalid")
+    input.classList.add("valid")
+    alertas[index].classList.remove('visivel')
+  }
+})})
+
+
+/*
+const form = document.getElementById('formulario')
 const campo = document.getElementById('campo')
 const alerta = document.getElementById('alerta')
 
@@ -18,5 +54,5 @@ form.addEventListener('change', function (event) {
   if (campo.value !== ""){
     campo.classList.remove('invalid')
     campo.classList.add('valid')
-  }
-})
+    alerta.innerHTML = ""}
+}) */
