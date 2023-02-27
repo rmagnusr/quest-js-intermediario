4437e626b0e614df92d085034423209ee3e2829c
rmagnusr <rmagnusdarosa@gmail.com>
2023-02-26 23:55:20 -0300

    adicionado readme e retirados os comentários do js

---
+++ readme.md
@@ -0,0 +1,34 @@
+# Formulário com Javascript
+
+## Validações para campos obrigatórios
+
+### Status do projeto
+
+Concluído!
+
+### Features
+
+- [x] Validação em verde ao preencher um campo
+- [x] Alerta em vermelho ao esvaziar um campo
+- [x] Alerta em vermelho ao tentar enviar um formulário com um campo vazio
+
+### Demonstração da aplicação
+
+https://rmagnusr.github.io/quest-js-intermediario/
+
+### Pré-requisitos
+
+Projeto web sem necessidade de instalações específicas. 
+
+### Tecnologias
+
+As seguintes ferramentas foram usadas na construção do projeto:
+- [JavaScript] (https://www.javascript.com/)
+- [HTML5]
+- [CSS3]
+
+### Autor 
+Renato Magnus
+- [Linkedin](https://www.linkedin.com/in/renato-magnus/)
+- [GitHub](https://github.com/rmagnusr)
+
--- src/javaScript/index.js
+++ src/javaScript/index.js
@@ -1,5 +1,3 @@
-//coletando os elementos
-
 const form = document.getElementById('formulario')
 const campos = document.querySelectorAll('.campo')
 const alertas = document.querySelectorAll('.alerta')
@@ -33,26 +31,3 @@ input.addEventListener('input', function (event){
     alertas[index].classList.remove('visivel')
   }
 })})
-
-
-/*
-const form = document.getElementById('formulario')
-const campo = document.getElementById('campo')
-const alerta = document.getElementById('alerta')
-
-form.addEventListener('submit', function (event) {
-
-  if (campo.value == ""){
-  event.preventDefault()
-  campo.classList.add('invalid')
-  alerta.innerHTML = "Campo obrigatório!"}
-  
-})
-
-
-form.addEventListener('change', function (event) {
-  if (campo.value !== ""){
-    campo.classList.remove('invalid')
-    campo.classList.add('valid')
-    alerta.innerHTML = ""}
-}) */
